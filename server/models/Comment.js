2c5890e74abc4b832dea310c6e5066c717026089
KartikeyKakaria <kartikeykakaria2@gmail.com>
2023-05-18 20:27:07 +0530

    added routes for videos and comments as well

---
+++ server/models/Comment.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+const CommentSchema = new Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    videoId:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    }
+}, { timestamps: true })
+
+export default model("comment", CommentSchema)
