aa433cb0bb28a47b6b885023e3176baf0453b6a3
KartikeyKakaria <kartikeykakaria2@gmail.com>
2023-05-29 20:50:35 +0530

    added basic logic for video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,7 +1,16 @@
-import {Router} from "express"
-import {  } from "../controllers/video.js"
-
+import { Router } from "express"
+import { addVideo, deleteVideo, updateVideo, getVideo, trend, addView, random ,sub } from "../controllers/video.js"
+import { verifyToken } from '../verifyToken.js'
 const videoRouter = Router();
 
+//create a video
+videoRouter.post("/", verifyToken, addVideo)
+videoRouter.delete("/:id", verifyToken, deleteVideo)
+videoRouter.put("/:id", verifyToken, updateVideo)
+videoRouter.get("/find/:id", getVideo)
+videoRouter.put("/view/:id", addView)
+videoRouter.get("/trend", trend)
+videoRouter.get("/random", random)
+videoRouter.get("/sub",verifyToken, sub)
 
 export default videoRouter;
