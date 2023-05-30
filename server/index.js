fab4dcc1e99af97f8fd7d3d3e28e3e2e36d639eb
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2023-05-30 18:41:31 +0530

    added search and tags routes for videos

--- server/index.js
+++ server/index.js
@@ -1,44 +1,57 @@
-import { config } from "dotenv"
-config()
-import express from "express"
-import { connect } from "mongoose"
-import userRouter from "./routes/users.js"
-import videoRouter from "./routes/videos.js"
-import commentRouter from "./routes/comments.js"
-import authRouter from "./routes/auth.js"
-import cookieParser from "cookie-parser"
+import { config } from "dotenv";
+config();
+import express from "express";
+import { connect } from "mongoose";
+import userRouter from "./routes/users.js";
+import videoRouter from "./routes/videos.js";
+import commentRouter from "./routes/comments.js";
+import authRouter from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
-const app = express()
-const PORT = process.env.PORT || 8000
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 const dbConn = () => {
-    connect(process.env.MONGO_URI)
-        .then(() => console.log("connected to DB"))
-        .catch(err => {
-            throw new Error(err)
-        })
-}
+  connect(process.env.MONGO_URI)
+    .then(() => console.log("connected to DB"))
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+//see all the incoming requests in the terminal
+app.use((req, res, next) => {
+  console.log(
+    `Requesting Method:[${req.method}] -> ${req.url} from [${req.socket.remoteAddress}]`
+  );
+  res.on("finish", () => {
+    console.log(
+      `Responding Method:[${req.method}] -> ${req.url} from [${req.socket.remoteAddress}] status:[${res.statusCode}]`
+    );
+  });
+  next()
+});
 
-app.use("/api/users", userRouter)
-app.use("/api/videos", videoRouter)
-app.use("/api/comments", commentRouter)
-app.use("/api/auth", authRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/users", userRouter);
+app.use("/api/videos", videoRouter);
+app.use("/api/comments", commentRouter);
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong"
-    return res.status(status).json({
-        sucess: false,
-        status,
-        message,
-    })
-})
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    sucess: false,
+    status,
+    message,
+  });
+});
 
 app.listen(PORT, () => {
-    dbConn()
-    console.log(`listening at port: ${PORT}`);
-})
+  dbConn();
+  console.log(`listening at port: ${PORT}`);
+});
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,16 +1,47 @@
-import { Router } from "express"
-import { addVideo, deleteVideo, updateVideo, getVideo, trend, addView, random ,sub } from "../controllers/video.js"
-import { verifyToken } from '../verifyToken.js'
+import { Router } from "express";
+import {
+  addVideo,
+  deleteVideo,
+  updateVideo,
+  getVideo,
+  trend,
+  addView,
+  random,
+  sub,
+  getByTag,
+  search,
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 const videoRouter = Router();
 
 //create a video
-videoRouter.post("/", verifyToken, addVideo)
-videoRouter.delete("/:id", verifyToken, deleteVideo)
-videoRouter.put("/:id", verifyToken, updateVideo)
-videoRouter.get("/find/:id", getVideo)
-videoRouter.put("/view/:id", addView)
-videoRouter.get("/trend", trend)
-videoRouter.get("/random", random)
-videoRouter.get("/sub",verifyToken, sub)
+videoRouter.post("/", verifyToken, addVideo);
+
+//delete a video
+videoRouter.delete("/:id", verifyToken, deleteVideo);
+
+//update a video
+videoRouter.put("/:id", verifyToken, updateVideo);
+
+//get a video
+videoRouter.get("/find/:id", getVideo);
+
+//add view to a video
+videoRouter.put("/view/:id", addView);
+
+//get the most viewed videos
+videoRouter.get("/trend", trend);
+
+//get any random videos
+videoRouter.get("/random", random);
+
+//get the videos of channels user is subscribed to
+videoRouter.get("/sub", verifyToken, sub);
+
+//get videos by tags
+videoRouter.get("/tags", getByTag);
+
+//get search results for videos
+videoRouter.get("/search", search);
 
 export default videoRouter;
